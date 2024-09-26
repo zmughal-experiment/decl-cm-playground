92f20383ce663f705a15028c58afa573f0de545e
Zakariyya Mughal <zaki.mughal@gmail.com>
2024-09-25 21:02:49 -0400

    refactor(package-file): Extract `rpm` query to module

---
+++ strategy/package-file/lib/PackageFile/Platform/RPM/RpmQuery.pm
@@ -0,0 +1,36 @@
+package PackageFile::Platform::RPM::RpmQuery;
+# ABSTRACT: Uses rpm queries to retrieve packages and files
+
+use strict;
+use warnings;
+
+sub extract {
+    my $package_dir = '/var/lib/rpm';
+
+    warn "Starting Fedora extraction...\n";
+
+    if (!-d $package_dir) {
+        die "Error: $package_dir does not exist or is not a directory\n";
+    }
+
+    # Get list of all installed packages
+    my @packages = `rpm -qa --dbpath $package_dir`;
+    chomp @packages;
+
+    for my $package (@packages) {
+        #warn "Processing package: $package\n";
+        my @files = `rpm -ql $package 2>&1`;
+        if ($? != 0) {
+            warn "Error running rpm command for $package: $!\n";
+            next;
+        }
+        chomp @files;
+        for my $file (@files) {
+            print "$package:$file\n";
+        }
+    }
+
+    warn "Fedora extraction completed.\n";
+}
+
+1;
