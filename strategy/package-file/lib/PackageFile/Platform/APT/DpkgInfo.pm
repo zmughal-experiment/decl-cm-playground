8c1ae40ef8b90b401cdc04607d64dfa229c48fab
Zakariyya Mughal <zaki.mughal@gmail.com>
2024-09-25 21:39:45 -0400

    refactor(package-file): Extract `dpkg` info file approach to module

---
+++ strategy/package-file/lib/PackageFile/Platform/APT/DpkgInfo.pm
@@ -0,0 +1,27 @@
+package PackageFile::Platform::APT::DpkgInfo;
+# ABSTRACT: Uses dpkg info files to retrieve packages and files
+
+use strict;
+use warnings;
+use File::Find;
+
+sub extract {
+    my $package_dir = '/var/lib/dpkg/info';
+
+    find(
+        sub {
+            return unless -f $_ && $_ =~ /\.list$/;
+            my $package = $_;
+            $package =~ s/\.list$//;
+            open my $fh, '<', $_ or die "Cannot open file $_: $!";
+            while (my $line = <$fh>) {
+                chomp $line;
+                print "$package:$line\n";
+            }
+            close $fh;
+        },
+        $package_dir
+    );
+}
+
+1;
