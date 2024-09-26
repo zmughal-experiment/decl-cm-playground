ec6a5f3508243fb3876c31ef2446b916865d0b2f
Zakariyya Mughal <zaki.mughal@gmail.com>
2024-09-25 20:44:22 -0400

    refactor(package-file): Extract dpkg-query to module

---
+++ strategy/package-file/lib/PackageFile/Platform/APT/DpkgQuery.pm
@@ -0,0 +1,25 @@
+package PackageFile::Platform::APT::DpkgQuery;
+# ABSTRACT: Uses dpkg-query to retrieve packages and files
+
+use strict;
+use warnings;
+
+sub extract {
+    my @packages = split /\n/, `dpkg-query -f '\${Package}\n' -W`;
+    my $total_packages = scalar @packages;
+    my $processed = 0;
+
+    for my $package (@packages) {
+        $processed++;
+        my @files = split /\n/, `dpkg -L $package`;
+        for my $file (@files) {
+            next if $file =~ /^package diverts others to:\ /;
+            print "$package:$file\n";
+        }
+        warn sprintf("Progress: %d/%d packages processed\n", $processed, $total_packages) if $processed % 100 == 0;
+    }
+
+    warn "Finished processing $total_packages packages.\n";
+}
+
+1;
